b1a9ebf6ef8690e2ab616ab37a15525bdebd1551
yugdassani <ydassani@gmail.com>
2019-02-09 15:38:35 -0500

    Able to cancel creation of idea

--- app/src/main/java/cs408/incubator/AddIdeaActivity.kt
+++ app/src/main/java/cs408/incubator/AddIdeaActivity.kt
@@ -1,16 +1,19 @@
 package cs408.incubator
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_add_idea.*
 
 class AddIdeaActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_idea)
 
         setSupportActionBar(addIdeaToolbar)
@@ -19,5 +22,31 @@ class AddIdeaActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_back)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_idea,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            return when(item.itemId){
+                R.id.confirm -> {
+                    val intent = Intent(this,MainIdeasActivity::class.java)
+                    setResult(1,intent)
+                    finish()
+                    true
+                }
+                android.R.id.home -> {
+                    val intent = Intent(this,MainIdeasActivity::class.java)
+                    setResult(-1,intent)
+                    finish()
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/cs408/incubator/MainIdeasActivity.kt
+++ app/src/main/java/cs408/incubator/MainIdeasActivity.kt
@@ -14,6 +14,7 @@ import android.support.v4.util.Pair
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
 import com.woxthebox.draglistview.DragListView
 import kotlinx.android.synthetic.main.activity_main_ideas.*
 import android.widget.Toast
@@ -22,6 +23,9 @@ import com.woxthebox.draglistview.DragItemAdapter
 
 
 class MainIdeasActivity : AppCompatActivity() {
+    val REQ_CODE = 1
+    private lateinit var mDragList: DragListView
+    private lateinit var ideaArray: ArrayList<Pair<Long,String>>
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +38,7 @@ class MainIdeasActivity : AppCompatActivity() {
             setHomeAsUpIndicator(R.drawable.ic_menu)
         }
 
-        val mDragList = findViewById<DragListView>(R.id.ideaList)
+        mDragList = findViewById<DragListView>(R.id.ideaList)
 
         mDragList.setDragListListener(object : DragListView.DragListListener {
             override fun onItemDragging(itemPosition: Int, x: Float, y: Float) {
@@ -52,11 +56,7 @@ class MainIdeasActivity : AppCompatActivity() {
             }
         })
 
-        var ideaArray = ArrayList<Pair<Long,String>>()
-        for(i in 0..25){
-            ideaArray.add(Pair(i.toLong(), "Idea-$i"))
-        }
-
+        ideaArray = ArrayList()
         mDragList.setLayoutManager(LinearLayoutManager(applicationContext))
         val listAdapter = IdeaItemAdapter(ideaArray,R.layout.idea_item,true)
         mDragList.setAdapter(listAdapter,true)
@@ -65,8 +65,34 @@ class MainIdeasActivity : AppCompatActivity() {
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { view ->
             val intent = Intent(this,AddIdeaActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent,REQ_CODE)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQ_CODE){
+            if(resultCode == 1){
+                val layout = findViewById<LinearLayout>(R.id.linList)
+                layout.removeAllViews()
+                val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT)
+                layout.addView(mDragList,params)
+                val newArray = ArrayList<Pair<Long,String>>()
+                newArray.add(Pair(111,"New Idea-sbfabif"))
+                newArray.addAll(ideaArray)
+
+
+                mDragList.setLayoutManager(LinearLayoutManager(applicationContext))
+                val listAdapter = IdeaItemAdapter(newArray,R.layout.idea_item,true)
+                mDragList.setAdapter(listAdapter,true)
+                mDragList.setCanDragHorizontally(false)
+                ideaArray.add(Pair(111,"New Ideda-sbfabif"))
+            }
+            else if(resultCode == -1){
+                Toast.makeText(applicationContext,"No idea added",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 }

9feaaf65d6c92e054427b74e2de26d171636c9d7
ShauralP <patel675@purdue.edu>
2019-02-09 16:42:29 -0500

    Created onclick method to add tag to idea in firestore

--- app/src/main/java/cs408/incubator/Tags.java
+++ app/src/main/java/cs408/incubator/Tags.java
@@ -2,6 +2,16 @@ package cs408.incubator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tags extends AppCompatActivity {
 
@@ -9,5 +19,19 @@ public class Tags extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags);
+
+    }
+
+    public void onClickbtnAddTag(View v)
+    {
+        EditText et_tag = (EditText)findViewById(R.id.et_tag);
+        Map<String, Object> tag_map = new HashMap<>();
+        tag_map.put(et_tag.getText().toString(), true);
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Ideas").document("irWRcr2YIjDxi2kgEn93")
+                .set(tag_map, SetOptions.merge());
+        Toast.makeText(this, "Tag has been added to Idea.", Toast.LENGTH_LONG).show();
     }
 }

b9c589896083bf66ad43aa49bc9c3b5a2fafd8f9
chachachoco <namchanipa@gmail.com>
2019-02-28 00:06:20 -0500

    attach image

---
+++ app/src/main/java/cs408/incubator/FullscreenImageActivity.java
@@ -0,0 +1,13 @@
+package cs408.incubator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullscreenImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen_image);
+    }
+}
---
+++ app/src/main/java/cs408/incubator/image_list.java
@@ -0,0 +1,4 @@
+package cs408.incubator;
+
+public class image_list {
+}

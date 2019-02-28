7a3416fcd77622c8bb8770743d8d54bcb03133b4
ShauralP <patel675@purdue.edu>
2019-02-28 09:41:12 -0500

    added verification for unique title

--- app/src/main/java/cs408/incubator/AddNoteActivity.java
+++ app/src/main/java/cs408/incubator/AddNoteActivity.java
@@ -2,6 +2,7 @@ package cs408.incubator;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
@@ -14,25 +15,31 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import firestore_library.FirestoreLibraryKt;
 
 public class AddNoteActivity extends AppCompatActivity {
     String idea_id = "";
-//    String USERNAME = FirestoreLibraryKt.getUSERNAME();
+    //    String USERNAME = FirestoreLibraryKt.getUSERNAME();
     String USERNAME = "User1";
     Boolean new_note = true;
     EditText tv_title = null;
     EditText tv_desc = null;
     String old_title = "";
     String old_desc = "";
+    List<String> title_list = null;
 
 
     @Override
@@ -49,7 +56,7 @@ public class AddNoteActivity extends AppCompatActivity {
         idea_id = i.getStringExtra("ideaID");
         new_note = i.getBooleanExtra("new_note", true);
 
-        if(!new_note) {
+        if (!new_note) {
             // TODO set edit text values
             String tit = i.getStringExtra("title");
             String des = i.getStringExtra("desc");
@@ -59,6 +66,21 @@ public class AddNoteActivity extends AppCompatActivity {
             old_desc = des;
         }
 
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final DocumentReference docRef = db.collection("Notes").document(idea_id);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, String> map_user_notes = (Map<String, String>) document.get(USERNAME);
+                        title_list = new ArrayList<String>(map_user_notes.keySet());
+                    }
+                }
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -67,6 +89,7 @@ public class AddNoteActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_add_note, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -75,7 +98,7 @@ public class AddNoteActivity extends AppCompatActivity {
                 final FirebaseFirestore db = FirebaseFirestore.getInstance();
                 final DocumentReference docRef = db.collection("Notes").document(idea_id);
 
-                String title = tv_title.getText().toString();
+                final String title = tv_title.getText().toString();
                 String desc = tv_desc.getText().toString();
 
                 if (title.length() == 0) {
@@ -89,9 +112,13 @@ public class AddNoteActivity extends AppCompatActivity {
                 }
 
                 // check if title is unique
+                if(title_list != null && title_list.contains(title)) {
+                    Toast.makeText(this, "Must have a unique title.", Toast.LENGTH_SHORT).show();
+                    return super.onOptionsItemSelected(item);
+                }
 
                 // check if content changed
-                if((!new_note) && title.equals(old_title) && desc.equals(old_desc)) {
+                if ((!new_note) && title.equals(old_title) && desc.equals(old_desc)) {
                     // title and desc are the same
                     NavUtils.navigateUpFromSameTask(this);
                     return true;

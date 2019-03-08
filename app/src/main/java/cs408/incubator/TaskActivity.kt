2293fc4b52fbdb74251d188fc208b8baaa2d8e8e
yugdassani <ydassani@gmail.com>
2019-03-07 22:50:38 -0500

    Deadlines can be rescheduled. Cancels notifications if task completed/deleted

--- app/src/main/java/cs408/incubator/TaskActivity.kt
+++ app/src/main/java/cs408/incubator/TaskActivity.kt
@@ -1,9 +1,6 @@
 package cs408.incubator
 
-import android.app.AlarmManager
-import android.app.DatePickerDialog
-import android.app.Notification
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
@@ -11,6 +8,7 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.content.ContextCompat.getSystemService
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -148,34 +146,97 @@ class TaskActivity : AppCompatActivity() {
     }
 
     fun updateItem(item: ToDoItem) {
+        var cal = Calendar.getInstance()
+        val current = cal.timeInMillis
         val dialog = AlertDialog.Builder(this)
         dialog.setTitle("Update ToDo Item")
         val view = layoutInflater.inflate(R.layout.dialog_dashboard, null)
         val toDoName = view.findViewById<EditText>(R.id.ev_todo)
+        val deadline = view.findViewById<EditText>(R.id.deadline)
+
         toDoName.setText(item.itemName)
+        deadline.setText(item.deadline)
+
+        deadline.setOnClickListener {
+
+            val listener = DatePickerDialog.OnDateSetListener { datePicker, y, m, d ->
+                cal.set(Calendar.YEAR,y)
+                cal.set(Calendar.MONTH,m)
+                cal.set(Calendar.DAY_OF_MONTH,d)
+
+                if(cal.timeInMillis < current){
+                    Toast.makeText(this,"Deadline cannot be set!",Toast.LENGTH_SHORT).show()
+                    cal = Calendar.getInstance()
+                }
+                else {
+                    val format = "MMM d, yyyy"
+                    val sdf = SimpleDateFormat(format, Locale.US)
+                    deadline.setText(sdf.format(cal.time))
+                }
+            }
+            DatePickerDialog(this,listener,cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
         val db = FirebaseFirestore.getInstance()
         val docRef = db.collection("Ideas").document(IDEA_ID)
-        docRef.update("Tasks", FieldValue.arrayRemove(item.itemName))
+        if(item.deadline.isNotEmpty()){
+            val taskName =  "${item.itemName}~${item.notifid}~${item.deadline}"
+            docRef.update("Tasks", FieldValue.arrayRemove(taskName))
+        }
+        else
+            docRef.update("Tasks", FieldValue.arrayRemove(item.itemName))
+
         dialog.setView(view)
         dialog.setPositiveButton("Update") { _: DialogInterface, _: Int ->
             if (toDoName.text.isNotEmpty()) {
+                if(deadline.text.isNullOrEmpty()) {
+                    item.itemName = toDoName.text.toString()
+                    //add new item
+                    docRef.update("Tasks", FieldValue.arrayUnion(toDoName.text.toString()))
+                    var str_log = item.itemName + "_to_" + toDoName.text.toString()
+                    docRef.update("Log", FieldValue.arrayUnion(genLogStr(USERNAME, "update", "task", str_log)))
 
-                item.itemName = toDoName.text.toString()
-                //add new item
-                docRef.update("Tasks", FieldValue.arrayUnion(toDoName.text.toString()))
-                var str_log = item.itemName + "_to_" + toDoName.text.toString()
-                docRef.update("Log", FieldValue.arrayUnion(genLogStr(USERNAME, "update", "task", str_log)))
-
-                item.toDoId = todoId
-                item.isCompleted = false
-                //dbHandler.updateToDoItem(item)
-                readTasks()
+                    item.toDoId = todoId
+                    item.isCompleted = false
+                    //dbHandler.updateToDoItem(item)
+                    readTasks()
+                }
+                else{
+                    reschedNotification(toDoName.text.toString(),deadline.text.toString(),cal.timeInMillis,item.notifid)
+                }
             }
         }
         dialog.setNegativeButton("Cancel") { _: DialogInterface, _: Int ->
 
         }
         dialog.show()
+    }
+
+    fun reschedNotification(name:String,deadline:String,delay: Long,id:Long){
+        val notification = getNotification(name)
+        val notif = Intent(this,NotificationPublisher::class.java).apply {
+            putExtra("notification",notification)
+            putExtra("notification-id",id)
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(this,0,notif,PendingIntent.FLAG_UPDATE_CURRENT)
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.set(AlarmManager.RTC,delay,pendingIntent)
+
+        val item = ToDoItem()
+        item.itemName = name
+        val notifData = "$name~$id~$deadline"
+        //add to firebase
+        // Access a Cloud Firestore instance from your Activity
+        val db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("Ideas").document(IDEA_ID)
+        docRef.update("Tasks", FieldValue.arrayUnion(notifData))
+        docRef.update("Log", FieldValue.arrayUnion(genLogStr(USERNAME, "updated", "task", notifData)))
+        item.toDoId = todoId
+        item.isCompleted = false
+
+        // dbHandler.addToDoItem(item)
+        readTasks()
     }
 
     fun addTask(name : String, deadline: String, delay: Long) {
@@ -210,11 +271,15 @@ class TaskActivity : AppCompatActivity() {
     }
 
     fun getNotification(content : String):Notification {
+        val i = Intent(this,MainIdeasActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0,i,0)
         val builder = NotificationCompat.Builder(this,"INCUBATOR")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Deadline Coming up")
                 .setContentText(content)
+                .setContentIntent(pendingIntent)
                 .setPriority(NotificationManagerCompat.IMPORTANCE_DEFAULT)
+                .setAutoCancel(true)
 
         return builder.build()
 
@@ -267,28 +332,33 @@ class TaskActivity : AppCompatActivity() {
                 docRefc.update("ctasks", FieldValue.arrayUnion(list[p1].itemName))
                 docRefc.update("Log", FieldValue.arrayUnion(genLogStr(USERNAME, "check", "task", list[p1].itemName)))
 
-                //add to clist for ui
-
-
-                //clist.add(list[p1].itemName)
-
-                //remove from the firebase task list
-                //activity.dbHandler.updateToDoItem(list[p1])
+                var taskname = list[p1].itemName
+                if(list[p1].deadline.isNotEmpty()){
+                    taskname+="~${list[p1].notifid}~${list[p1].deadline}"
+                    val nm = activity.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    nm.cancel(list[p1].notifid.toInt())
+                }
                 val db = FirebaseFirestore.getInstance()
                 val docRef = db.collection("Ideas").document(IDEA_ID)
-                docRef.update("Tasks", FieldValue.arrayRemove(list[p1].itemName))
+                docRef.update("Tasks", FieldValue.arrayRemove(taskname))
                 activity.readTasks()
             }
+
             holder.delete.setOnClickListener {
                 val dialog = AlertDialog.Builder(activity)
                 dialog.setTitle("Are you sure")
                 dialog.setMessage("Do you want to delete this item ?")
                 dialog.setPositiveButton("Continue") { _: DialogInterface, _: Int ->
                     //remove
+                    var taskname = list[p1].itemName
+                    if(list[p1].deadline.isNotEmpty()){
+                        taskname+="~${list[p1].notifid}~${list[p1].deadline}"
+                        val nm = activity.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                        nm.cancel(list[p1].notifid.toInt())
+                    }
                     val db = FirebaseFirestore.getInstance()
                     val docRef = db.collection("Ideas").document(IDEA_ID)
-                    Toast.makeText(activity,list[p1].itemName,Toast.LENGTH_SHORT).show()
-                    docRef.update("Tasks", FieldValue.arrayRemove(list[p1].itemName))
+                    docRef.update("Tasks", FieldValue.arrayRemove(taskname))
                             .addOnSuccessListener {
                                 docRef.update("Log", FieldValue.arrayUnion(genLogStr(USERNAME, "delete", "task", list[p1].itemName)))
 
@@ -344,6 +414,8 @@ class TaskActivity : AppCompatActivity() {
                     if(task.contains("~")){
                         val parts = task.split("~")
                         item.itemName = parts[0]
+                        item.notifid = parts[1].toLong()
+                        item.deadline = parts[2]
                     }
                     else {
                         item.itemName = task
